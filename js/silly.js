81f6b755499199c43a6bd413ff7ee97fb97864f3
unknown <EE208118@lpz-ee208118.sasken.com>
2014-11-06 18:50:31 +0530

    Added geolocation call to Do It button

--- js/silly.js
+++ js/silly.js
@@ -1,6 +1,21 @@
 var SILLY = (function(module) {
 	module.DoIt = function(resultObject) {
+		navigator.geolocation.getCurrentPosition(geoLocationSuccess,
+							geoLocationError,
+							{timeout:30000});
 		resultObject.prepend(Date() + '<br/>');
 	};
+
+	function geoLocationSuccess(position){
+		resultObject.prepend(position.coords.latitide + postion.coords.longitude + '<br/>');
+	};
+
+	function geoLocationError(error){
+		resultObject.prepend('error: ' + error.message);
+	};
+
 	return module;
 }(SILLY || {}));
+
+
+	
